45f8439c0c21d295bf1a073635edce907ee3b7f8
Igor Nikolaev <INikolaev@domain.corp>
2018-03-05 12:48:28 +0300

    Setup a repo

---
+++ My_Csharp_app/My_Csharp_app/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace My_Csharp_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Just to test that i can interact with git and CI");
+        }
+    }
+}
